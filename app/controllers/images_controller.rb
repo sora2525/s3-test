ccbffc25b6bceaf910b5ee5b4cc1c871f82f41b4
sora2525 <sky0907a.a@gmail.com>
2025-01-16 14:52:55 +0900

    image

---
+++ app/controllers/images_controller.rb
@@ -0,0 +1,25 @@
+class ImagesController < ApplicationController
+    def index
+      @images = Image.all
+    end
+  
+    def new
+      @image = Image.new
+    end
+  
+    def create
+      @image = Image.new(image_params)
+      if @image.save
+        redirect_to images_path, notice: "画像がアップロードされました。"
+      else
+        render :new, alert: "画像のアップロードに失敗しました。"
+      end
+    end
+  
+    private
+  
+    def image_params
+      params.require(:image).permit(:title, :file)
+    end
+  end
+  
